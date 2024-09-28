e277ec527310f6fec01a0281478554b38513b7b0
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-09-28 17:41:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ let displayValue = '';
 function appendNumber(number) {
     displayValue += number;
     updateDisplay();
-    changeBackgroundColor();
 }
 
 function appendOperation(operation) {
     displayValue += ` ${operation} `;
     updateDisplay();
-    changeBackgroundColor();
 }
 
 function clearDisplay() {
     displayValue = '';
     updateDisplay();
-    changeBackgroundColor();
 }
 
 function calculateResult() {
@@ -25,15 +22,8 @@ function calculateResult() {
         displayValue = 'Error';
     }
     updateDisplay();
-    changeBackgroundColor();
 }
 
 function updateDisplay() {
     document.getElementById('display').value = displayValue;
-}
-
-function changeBackgroundColor() {
-    const colors = ['#FFCDD2', '#C5E1A5', '#FFE082', '#BBDEFB', '#FFAB91'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.querySelector('.calculator').style.backgroundColor = randomColor;
 }
